c43615941bc176797345992ca3c222dff563f4fa
kietimartin <martinkieti6@gmail.com>
2023-07-19 23:45:30 +0300

    last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,12 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 
 #we use modulo to get the last digit
-last_digit = number % 10
+last_digit = abs(number)
+last_digit = last_digit % 10
 
 #set the conditions
 #greater than 5
 if (last_digit > 5):
-    print('Last digit of {} is {} and is greater than 5'.format(number, last_digit))
+    if (number > 0):
+        print('Last digit of {} is {} and is greater than 5'.format(number, last_digit))
+    elif (number < 0):
+        print('Last digit of {} is -{} and is greater than 5'.format(number, last_digit))
 
 #is 0
 elif (last_digit == 0):
@@ -16,4 +20,7 @@ elif (last_digit == 0):
 
 #is less than 6 and not 0
 elif (last_digit < 6) and (last_digit != 0):
-    print('Last digit of {} is {} and is less than 6 and not 0'.format(number, last_digit))
+    if (number > 0):
+        print('Last digit of {} is {} and is less than 6 and not 0'.format(number, last_digit))
+    elif (number < 0):
+        print('Last digit of {} is -{} and is less than 6 and not 0'.format(number, last_digit))
