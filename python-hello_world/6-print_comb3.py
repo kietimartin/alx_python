a1dcc6523d5a55893fa7ca707642b5c841cc3b75
kietimartin <martinkieti6@gmail.com>
2023-07-20 15:00:04 +0300

    combinations

---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Combination og two numbers to form a single digit that is not repeated
+# asscending order
+# 2 loops only
+
+for i in range(0,10):
+    for j in range(0,10):
+        if (i != j) and ():
+            print('{}{}, '.format(i, j), end="")
+
+        
