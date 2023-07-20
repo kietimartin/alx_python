f404e3a464b9429f1a39427f999d44499c9954ae
kietimartin <martinkieti6@gmail.com>
2023-07-20 13:41:55 +0300

    00...99

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 for i in range(00,100):
-    i += 1
     if (i <= 98):
         print('{:02d}, '.format(i), end="")
     elif i == 99:
         print('{:02d}\n'.format(i))
-        break
-    
+        break   
+    i += 1
