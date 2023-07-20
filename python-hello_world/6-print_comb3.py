6ac2e5a941e602d5d996bbefbf12ee8691056dca
kietimartin <martinkieti6@gmail.com>
2023-07-20 22:43:59 +0530

    combinations

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -4,9 +4,9 @@
 # asscending order
 # 2 loops only
 
-for i in range(0,10):
-    for j in range(0,10):
-        if (i != j) and ():
-            print('{}{}, '.format(i, j), end="")
-
-        
+for i in range(100):
+    if(i / 10 != i % 10) and (i / 10 < i % 10):
+        if(i < 89):
+            print('{:02d}, '.format(i), end='')
+        else :
+            print('{:02d}'.format(i))
