8b896bfd27b39e00a83b7f3a12f4d8419a816a3f
kietimartin <martinkieti6@gmail.com>
2023-07-27 21:56:05 +0300

    Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -11,4 +11,4 @@ def print_matrix_integer(matrix=[[]]):
             if i == len(row) - 1:
                 print("{:d}".format(row[i]))
             else:
-                print("{:d}".format(row[i]), end=" ")
+                print("{:d}".format(row[i]), end=" "
