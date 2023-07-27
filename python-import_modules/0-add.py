e3e2b2b6bf7c6b621ff36de4e1e8b2ae99192f5e
kietimartin <martinkieti6@gmail.com>
2023-07-27 21:21:38 +0300

    Importing a simple function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
-import add_0.py as add
 
-a = add.add(1)
-b = add.add(2)
+# variables a and b
+a = 1
+b = 2
 
-print
+# the add function
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return a + b
+
+if __name__ == "__main__":
+    # Calculate the result of add(a, b)
+    result = add(a, b)
+
+    # Print the result using string format
+    print(f"{a} + {b} = {result}")
+    
