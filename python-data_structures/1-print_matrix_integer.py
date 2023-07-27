c65fc903492aaf1c369a5bf4390a5c3d3a152e43
kietimartin <martinkieti6@gmail.com>
2023-07-27 21:55:37 +0300

    Raising an exception

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,14 @@
+# !/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if not matrix or not matrix[0]:
+        # Check if the matrix is empty or contains an empty row
+        print()
+        return
+
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
