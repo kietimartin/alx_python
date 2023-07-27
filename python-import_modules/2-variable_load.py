f4577bb3d8f541ed42fe954487ec8a73bd148a03
kietimartin <martinkieti6@gmail.com>
2023-07-27 21:40:36 +0300

    Everything can be imported fixed

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    with open("variable_load_2.py", "r") as file:
-        contents = file.read()
-
-    variables = {}
-    exec(contents, variables)
-
-    if 'a' in variables:
-        print(variables['a'])
+    from variable_load_2 import a
+    print(a)
+    
