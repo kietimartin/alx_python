fe617241ce127ed5154228610b6a14f77ac01ba6
kietimartin <martinkieti6@gmail.com>
2023-08-03 12:38:38 +0300

    size validator

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -8,20 +8,37 @@ class Square:
     It also checks if the value is an integer and is greater than zero and raises an execption
     """
     def __init__(self, size=0):
-        # Private attribute
-        pass
-        # self.__size = size
+        """
+        Initialize the Square object with a size (default is 0).
+        """
+        self.__size = self.integer_checker(size)
+        self.greater_than_zero()
 
-    def integer_checker(self):
-        if self.__size.is_integer():
-            self.__size = size
-            
-        else:
-            raise TypeError('size must be an integer')
+    def get_size(self):
+        """
+        Get the size of the square.
+        """
+        return self.__size
+    
+    def set_size(self, size):
+        """
+        Set the size of the square after checking if the value is a positive integer.
+        """
+        self.__size = self.integer_checker(size)
+        self.greater_than_zero()
+
+    def integer_checker(self, value):
+        """
+        Check if the given value is an integer. If not, raise a TypeError.
+        """
+        if not isinstance(value, int):
+            raise TypeError('Size must be an integer')
+        return value
         
     def greater_than_zero(self):
-        if (self.__size >= 0):
-            self.__size = size
-        else:
-            raise ValueError('size must be >= 0')
+        """
+        Check if the size is greater than or equal to 0. If not, raise a ValueError.
+        """
+        if self.__size < 0:
+            raise ValueError('Size must be >= 0')
         
