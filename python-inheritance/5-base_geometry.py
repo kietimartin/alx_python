6a588b68eb5fb51c83bda4e267c4452c3b408dd3
kietimartin <martinkieti6@gmail.com>
2023-08-03 23:13:57 +0300

    Geometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,28 @@
+'''
+This is task 5
+'''
+class BaseGeometry:
+    def area(self):
+        """Define Area function.
+
+        Raises:
+            Exception: if area is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate an integer value.
+
+        Args:
+            name (str): Name of the value being validated.
+            value (int): Value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
