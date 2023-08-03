d9bbad5352d4e4164224d1c5cb0782b2f0e6e00d
kietimartin <martinkieti6@gmail.com>
2023-08-03 12:30:09 +0300

    size validator

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,17 +9,19 @@ class Square:
     """
     def __init__(self, size=0):
         # Private attribute
-        self.__size = size
+        pass
+        # self.__size = size
 
     def integer_checker(self):
         if self.__size.is_integer():
-            return self.__size
+            self.__size = size
+            
         else:
             raise TypeError('size must be an integer')
         
     def greater_than_zero(self):
         if (self.__size >= 0):
-            return self.__size
+            self.__size = size
         else:
             raise ValueError('size must be >= 0')
         
