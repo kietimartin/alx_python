b20669bffa83b6abd9d11353de9879ba7e0a2159
kietimartin <martinkieti6@gmail.com>
2023-08-03 23:06:16 +0300

    Inherit from

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,9 @@
+"""
+This is task 2 of the inheritance project\
+"""
+def inherits_from(obj, a_class):
+    """
+    the object is an instance of a class
+    function that returns True if the object
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
