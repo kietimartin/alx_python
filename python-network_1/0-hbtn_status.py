8428714ce338c807ebe8817ef1332890c8fb275a
kietimartin <martinkieti6@gmail.com>
2023-08-10 23:04:19 +0300

    Python network

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A script for fetches https://alx-intranet.hbtn.io/status status
+"""
+
+import requests
+
+if __name__ == '__main__':
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    result = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(result.text)))
+    print("\t- content: {}".format(result.text))
---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A script that:
+- sends a request to the URL and displays the value of the variable
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    result = response.headers.get("X-Request-Id")
+    print(result)
