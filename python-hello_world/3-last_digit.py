5630f4c8bdbefca8f7e2765736a6b97e0f7a6f89
kietimartin <martinkieti6@gmail.com>
2023-08-20 04:17:22 -0700

    Last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,7 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 
 #we use modulo to get the last digit
+number = str(number)
 last_digit = number[:-1]
+last_digit = int(last_digit)
 
 #set the conditions
 #greater than 5
