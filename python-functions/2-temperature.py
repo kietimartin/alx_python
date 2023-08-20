4dce1e96992b1eb3e2d0457530a470adbf473e16
kietimartin <martinkieti6@gmail.com>
2023-08-20 04:53:56 -0700

    Temperature converter

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def convert_to_celsius(fahrenheit):
-    if (fahrenheit < 0)
+    if (fahrenheit < 0):
         fahrenheit = round((fahrenheit - 32) * (5/9), 2)
         return fahrenheit
     
