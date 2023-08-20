12ac1e3b1ced39f3c975f8310312d97012076f62
kietimartin <martinkieti6@gmail.com>
2023-08-20 04:23:51 -0700

    Last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,18 +7,19 @@ last_digit = abs(number)
 last_digit = last_digit % 10
 
 #set the conditions
-#greater than 5
+
 if (number > 0):
+    #greater than 5
     if (last_digit > 5):
         print('Last digit of {} is {} and is greater than 5'.format(number, last_digit))
 
-#is 0
-    elif (last_digit == 0):
-        print('Last digit of {} is {} and is 0'.format(number, last_digit))
-
-#is less than 6 and not 0
+    #is less than 6 and not 0
     elif (last_digit < 6) and (last_digit != 0):
         print('Last digit of {} is {} and is less than 6 and not 0'.format(number, last_digit))
 
-if (number < 0):
+elif (number < 0):
     print('Last digit of {} is -{} and is less than 6 and not 0'.format(number, last_digit))
+
+# if number is zero
+else:
+        print('Last digit of {} is {} and is 0'.format(number, last_digit))
