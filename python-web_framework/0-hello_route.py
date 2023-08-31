f51a99dfd11bdd3e7339850ec2b890a24005f2ee
martinkieti <martinkieti6@gmail.com>
2023-08-31 22:59:53 +0300

    Hello flask

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,14 +1,16 @@
-from flask import Flask
-'''THis script displays a text when a certain page is opened on our 
+
+'''This script displays a text when a certain page is opened on our 
  testing server on flask
 '''
+from flask import Flask
+
 app = Flask(__name__)
 
-# Defining the route
+'''Defining the route'''
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB'
 
-#  Starting the server
+'''Starting the server'''
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
