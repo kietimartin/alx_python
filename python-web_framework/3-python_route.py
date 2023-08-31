ac6552cd9a5fdc8b22687c2ccee4fb100067dbcb
martinkieti <martinkieti6@gmail.com>
2023-08-31 23:27:37 +0300

    Python is also cool!

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python3
+'''Basic flask web server
+-Creates a route on 0.0.0.0/5000
+-Added new route '/hbnb'
+-Added new route '/c/<text>'
+-Added the python route
+'''
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+text = "is cool"
+
+'''Defining the route'''
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+'''Added new route'''
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+'''Adding a new route'''
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return f'C {escape(text.replace("_", " "))}'
+
+'''Adding a new python route'''
+@app.route('/python/<text>', strict_slashes=False)
+def python_show_text():
+    return f'Python {escape(text.replace("_", " "))}'
+
+'''Starting the server'''
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
