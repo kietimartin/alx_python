651be24459f6386da703f96a693bca6628dc4074
martinkieti <martinkieti6@gmail.com>
2023-08-31 23:17:11 +0300

    C is fun!

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 '''Adding a new route'''
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    return 'C + text.replace("_", " ")'
+    return 'C + {escape(text.replace("_", " "))}'
 
 '''Starting the server'''
 if __name__ == '__main__':
