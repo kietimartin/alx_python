8f4693e8f78464d89bb488d3a5581a9aea5fd15d
martinkieti <martinkieti6@gmail.com>
2023-08-31 23:14:19 +0300

    C is fun!

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3
+'''Basic flask web server
+-Creates a route on 0.0.0.0/5000
+-Added new route '/hbnb'
+-Added new route '/c/<text>'
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+'''Defining the route'''
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+'''Added new route'''
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+'''Adding a new route'''
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return 'C + text.replace("_", " ")'
+
+'''Starting the server'''
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
