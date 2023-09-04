aa941b83ac49a01b3a0d3a3d34fdb5f5d89a47b6
martinkieti <martinkieti6@gmail.com>
2023-09-04 11:16:42 +0300

    Python is also cool!

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -1,26 +1,74 @@
-"""importing flask module"""
+#! /usr/bin/python3
+"""
+Basic Flask Web Server
+
+This script creates a simple Flask web server that defines routes to handle various URLs.
+- Root URL ("/") responds with a greeting message.
+- "/hbnb" URL responds with "HBNB".
+- "/c/<text>" URL responds with formatted text.
+- "/python/<text>" URL responds with Python-related text (default value: "is cool").
+
+This script requires Flask and markupsafe to be installed.
+
+Author:
+    Martin Kieti
+
+Date:
+    August 31, 2023
+"""
 from flask import Flask
-"""create an instance"""
+from markupsafe import escape
+
 app = Flask(__name__)
-"""route definitiion"""
+
+'''Defining the route'''
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB!"
-"""/hbnb: display “HBNB”"""
+    """
+    Root URL Route
+    
+    Responds with a greeting message.
+    """
+    return 'Hello HBNB!'
+
+'''Added new route'''
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """
+    /hbnb URL Route
+    
+    Responds with "HBNB".
+    """
+    return 'HBNB'
 
-"""C is fun!"""
+'''Adding a new route'''
 @app.route('/c/<text>', strict_slashes=False)
-def show_c(text):
-    return "C " + text.replace("_", " ")
+def c_is_fun(text):
+    """
+    /c/<text> URL Route
+    
+    Responds with formatted text.
+    
+    Args:
+        text (str): Text to be formatted.
+    """
+    return f'C {escape(text.replace("_", " "))}'
 
-"""Python is cool!"""
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+'''Adding a new python route'''
+@app.route('/python/<text>',
+           defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    return "Python " + text.replace("_", " ")
+def python_show_text(text):
+    """
+    /python/<text> URL Route
+    
+    Responds with Python-related text.
+    
+    Args:
+        text (str): Text related to Python.
+    """
+    return f'Python {escape(text.replace("_", " "))}'
 
+'''Starting the server'''
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
